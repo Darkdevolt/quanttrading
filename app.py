cc916ecbb243bba02ba14a45181329f2b383cc58
Darkdevolt <diopibrahimabdallah@gmail.com>
2025-04-23 23:59:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -132,11 +132,11 @@ def run_backtest(data, capital, params):
                 buy_prices = []  # Reset après vente
         
         # Condition d'entrée
-        entry_cond = (
+        entry_cond = 
             (data['Signal'].iloc[i] == 1) and  # Signal technique
             (cash > 0) and 
             (shares == 0) and 
-            (pending == 0))
+            (pending == 0)
         
         if entry_cond:
             max_shares = int(cash / (price * (1 + params['fees'])))
