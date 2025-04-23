26faae59861a947a82e0f673e13c7d0bd1c1ad20
Darkdevolt <diopibrahimabdallah@gmail.com>
2025-04-23 22:26:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -560,3 +560,4 @@ if uploaded_file is not None:
         ax7.plot(data.index, perf_strategie, label='Stratégie', linewidth=2)
         ax7.plot(data.index, perf_buy_hold, label='Buy & Hold', linewidth=2, linestyle='--')
         ax7.set_title('
+        
